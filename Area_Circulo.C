9abfe8580251a2ce8b66393fa09a64be62951deb
Luiz Marcos Abril Silva <104640447+ITSMEAJOKER@users.noreply.github.com>
2022-08-20 11:11:03 -0300

    Create Area_Circulo.C

---
+++ Area_Circulo.C
@@ -0,0 +1,17 @@
+ #include <stdio.h>
+
+ double main () {
+
+         double raio1, raio2,  n, a, 
+
+       printf("digite o raio1"); scanf("%lf", &raio1);
+
+       printf("digite o raio2"); scanf("%lf ", &raio2 ); 
+
+        x = 3.14159  * raio1 * raio2;
+
+        printf("A = %.4lf\n", A);
+
+        return 0 
+
+}
